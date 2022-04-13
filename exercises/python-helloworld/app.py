df0615be76eebd64593e1ed6972ff864f5f6a1c2
Tyler Blaszak <tab890@yahoo.com>
2022-04-12 21:49:51 -0400

    Revert "end of fundamentals lesson"
    This reverts commit 62c1b30faab137dc01d1fb7118dfbc35808dd962.

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,34 +1,9 @@
 from flask import Flask
-import json
-import logging
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    app.logger.info("Main request successful!")
     return "Hello World!"
 
-@app.route("/health")
-def status():
-    response = app.response_class(
-        response=json.dumps({"result":"OK - healthy"}),
-        status=200,
-        mimetype='application/json'
-    )
-    app.logger.info("Health check called successfully")
-    return response
-
-@app.route("/metrics")
-def metrics():
-    response = app.response_class(
-        response=json.dumps({"status":"success", "code": 0, "data": {"userCount": 140, "userCountActive": 23}}),
-        status=200,
-        mimetype='application/json'
-    )
-    app.logger.info("Metrics endpoint called successfully")
-    return response
-
 if __name__ == "__main__":
-    logging.basicConfig(filename='app.log', level=logging.DEBUG)
     app.run(host='0.0.0.0')
